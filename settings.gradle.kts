78c5cded472cc5c4df09ed08cefe2da193322ed7
ZooKong <prorew94@gmail.com>
2023-10-09 12:51:24 +0900

    init: project initialize
    Intellij를 통한 프로젝트 초기화

---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+rootProject.name = "zookong.auth"
+
+include(
+    "oauth2"
+
+
+)
+
+
+
+//include 'pntbiz-indoorplus-core'
+//include 'pntbiz-indoorplus-license-core'
+//include 'pntbiz-indoorplus-license-domain'
+//include 'pntbiz-indoorplus-license-admin'
